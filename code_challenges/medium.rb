c9cc9805abe8d30b3ad74a0e72eef64b1af6d8b2
David Marino <116821829+radsy207@users.noreply.github.com>
2022-11-03 10:52:53 -0400

    Update medium.rb

--- code_challenges/medium.rb
+++ code_challenges/medium.rb
@@ -5,6 +5,6 @@ if good_driving_record = true && age > 25
     puts "Qualify for discount"
 elsif good_driving_record = true || age > 25
     puts "Pay full price"
-else good_driving_record = false && age <25
+elsif good_driving_record = false && age <25
     puts "Requires cosigner"
 end
